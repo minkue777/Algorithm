988c719cbcd3e77966073354f067dd6023ccc666
mingyu.lee <mingyu.lee@nhnent.com>
2020-09-09 14:17:32 +0000

    New

---
+++ Programmers/.ipynb_checkpoints/Marathon-checkpoint.py
@@ -0,0 +1,15 @@
+import collections
+
+def solution(participant, completion):
+    temp = 0
+    dic = {}
+    for part in participant:
+        dic[hash(part)] = part
+        temp += int(hash(part))
+    for com in completion:
+        temp -= hash(com)
+    answer = dic[temp]
+
+    return answer
+
+    

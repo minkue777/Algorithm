1dd78d4051e91cc435de75b67f061ac139290ebf
LeeMingyu <minkue7777@naver.com>
2020-09-16 07:11:00 +0000

    .

---
+++ others/dijkstra.py
@@ -0,0 +1,51 @@
+import sys
+
+input = sys.stdin.readline
+INF = int(1e9) # 무한을 의미하는 값으로 10억 설정
+
+# 노드의 개수, 간선의 개수
+n, m = map(int, input().split())
+# 시작 노드 번호
+start = int(input())
+# 각 노드에 연결되어 있는 노드에 대한 정보를 담는 리스트 만들기
+graph = [[] for _ in range(n+1)]
+# 방문 체크 리스트
+visited = [False] * (n+1)
+# 최단 거리 테이블. 무한으로 초기화
+distance = [INF] * (n+1)
+
+# 모든 간선의 정보
+for _ in range(m):
+    # a에서 b로 가는 비용 c
+    a, b, c = map(int, input().split())
+    graph[a].append((b, c))
+    
+
+# 방문하지 않은 노드 중에서, 가장 최단 거리가 짧은 노드의 번호를 반환
+def get_smallest_node():
+    min_val = INF
+    index = 0
+    for i in range(1, n+1):
+        if distance[i] < min_val and not visited[i]:
+            min_value = distance[i]
+            index = i
+    return index
+
+
+def dijkstra(start):
+    distance[start] = 0
+    visited[start] = True
+    for j in graph[start]:
+        distance[j[0]] = j[1]
+    # 시작 노드를 제외한 전체 n-1개의 노드에 대해 반복
+    for i in range(n-1):
+        # 현재 최단 거리가 가장 짧은 노드를 꺼내서
+        now = get_smallest_node()
+        visited[now] = True
+        # 현재 노드와 연결된 다른 노드를 확인
+        for j in graph[now]:
+            cost = distance[now] + j[1]
+            # 현재 노드를 거쳐서 다른 노드로 이동하는 거리가 더 짧은 경우
+            if cost < distance[j[0]]:
+                distance[j[0]] = cost
+        

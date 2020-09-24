3ab7beaeb0870509a696778d3b8bb6d0c1385d18
LeeMingyu <minkue7777@naver.com>
2020-09-24 18:05:14 +0900

    .

--- sorting/HeapSort.py
+++ sorting/HeapSort.py
@@ -1,5 +1,6 @@
 import heapq
 
+
 def heapsort(iterable):
     h = []
     result = []
@@ -11,6 +12,7 @@ def heapsort(iterable):
     for i in range(len(h)):
         result.append(-heapq.heappop(h))
     return result
+
 
 result = heapsort([1, 3, 5, 7, 9, 2, 4, 6, 8, 0])
 print(result)
